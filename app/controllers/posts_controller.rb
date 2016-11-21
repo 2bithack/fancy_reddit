e4b91e54d6ed05370395882de894571599eb3fb6
Ross Justin <justrostin@gmail.com>
2016-11-21 15:30:11 -0800

    added new subreddit form on home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def index
       @posts = Post.all
       @subreddits = Subreddit.all
+      @subreddit = Subreddit.new
   end
   def new
       @subreddit = Subreddit.find(params[:subreddit_id])
