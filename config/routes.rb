e8dd175cee45c25e3ec165302d418da1df24582d
Ross Justin <justrostin@gmail.com>
2016-11-28 15:24:28 -0800

    added users and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :users
   resources :subreddits, only: [:show, :create, :new] do
       resources :posts, only: [:show, :new, :create]
 
@@ -6,6 +9,13 @@ Rails.application.routes.draw do
   resources :post, only: [:index] do
       resources :comments, only: [:create]
   end
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :sessions
+
   root to: 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
